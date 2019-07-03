6d64552464d9a3eb51068d0b0f80eabbf221a80f
Anu9-4Radha <41417049+Anu9-4Radha@users.noreply.github.com>
2019-07-03 18:22:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Anuradha, Lets retry!')
+  response.send('Hello Anuradha, Lets show demo to BG!')
 })
 
 app.listen(app.get('port'), function() {
