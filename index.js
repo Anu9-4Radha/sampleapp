f023d242174211e7accd4ee671932be4050b2457
Anu9-4Radha <41417049+Anu9-4Radha@users.noreply.github.com>
2019-07-03 17:46:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Anuradha, yes now web service is present on the azure portal!')
+  response.send('Hello Anuradha, Lets retry!')
 })
 
 app.listen(app.get('port'), function() {
