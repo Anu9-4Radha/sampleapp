3abe8978304a981e0a1bb4ca2b64471f1ae722f4
Anu9-4Radha <41417049+Anu9-4Radha@users.noreply.github.com>
2019-07-04 17:21:23 +0530

    Env addtion test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Anuradha, its evening and time to hurry to complete the task!')
+  response.send('Hello Anuradha, its evening , work fast')
 })
 
 app.listen(app.get('port'), function() {
