13d51049d247a79bea90e45906ce2e7b1a2a5e4a
Anu9-4Radha <41417049+Anu9-4Radha@users.noreply.github.com>
2019-07-04 17:02:15 +0530

    Updated the message on Webpage
    This is more informative message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Anuradha, Lets show demo to BG!')
+  response.send('Hello Anuradha, its evening and time to hurry to complete the task!')
 })
 
 app.listen(app.get('port'), function() {
